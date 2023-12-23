f05ac194a165e01808634ace75514389d0c319b3
Buda Suyasa <budasuyasa@gmail.com>
2023-12-23 17:10:59 +0800

    update dengan komentar

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const port = 3000;
 
 const knex = require('knex')(require('./knexfile').development)
 
+// menggunakan middleware agar request body dapat ditangkap oleh express via
+// Request (req) object
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// set ejs sebagai template engin default dari express
+// pastikan ejs sudah diinstall sebelumnya dengan:
+// npm install ejs --save
 app.set('view engine', 'ejs')
 
+// default root route atau route utama dari web server
 app.get('/', (req, res) => {
     res.send('Emang boleh?');
 });
 
 app.get('/profile', (req, res) => {
+    // merender template profile pada vies/profile.ejs
+    // method render menerima 2 buah parameter
+    // parameter pertama adalah nama template dan 
+    // parameter kedua adalah data yang bisa kita kirimkan
+    // ke template
     res.render('profile', {
         nama: 'Tugus',
         kelas: 'Pagi2'
@@ -22,38 +33,56 @@ app.get('/profile', (req, res) => {
 
 
 app.get('/buku', (req, res) => {
+    // mengambil data buku dari table buku dengan menggunakan knex
+    // sintaks dibawah akan diterjemahkan menjadi SQL query berikut:
+    // select * from buku
     knex('buku').select('*')
+        // promises ketika query yang dilakukan sukses
+        // maka hasil query akan tersedia pada variabel data
         .then((data)=> {
             console.log(data);
+
+            // render template dan bagikan data hasil query
             res.render('buku_index', {
                 buku: data
             })
         })
+        // jika terjadi error, tampilkan error pada console
         .catch((error)=>console.log(error))
 })
 
+// route untuk membuat data buku baru
 app.get('/buku/create', (req, res) => {
     res.render('buku_create')
 })
 
 app.post('/buku/simpan', function(req, res) {
+    // tangkap semua variabel yang dikirimkan dari form 
+    // melalui object req.body
     const judul = req.body.judul;
     const tahun_terbit = req.body.tahun_terbit;
     const pengarang = req.body.pengarang;
     
+    // insert data buku baru dengan method insert
+    // sesuai dengan kolom dan value yang diberikan dari
+    // form 
     knex('buku').insert({
       judul: judul,
       tahun_terbit: tahun_terbit,
       pengarang: pengarang,
 
     }).then(()=>{
+      // jika proses insert berhasil, kembalikan response pesan berhasil
       res.send('Data berhasil disimpan')
     }).catch((err) => {
+      // jika terjadi error tampilkan error pada console 
+      // serta kembalikan response pesan error
       console.log(err)
       res.send('Terjadi kesalah saat mennyimpan data')
     })
 });
 
+// jalankan aplikasi pada port yang telah ditentukan
 app.listen(port, () => {
     console.log(`Web server berjalan pada http://localhost:${port}`);
 });
