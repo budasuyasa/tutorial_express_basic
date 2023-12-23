6a1ca309689b3e0f23f4000e4b7e78a5c1c964c8
Buda Suyasa <budasuyasa@gmail.com>
2023-12-23 22:28:09 +0800

    Add database configuration and create book form

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+// file ini berisikan object konfigurasi koneksi database yang digunakan pada
+// module knex. Silahkan sesuaikan konfigurasi database yang Anda gunakan
 module.exports = {
   development: {
     client: "mysql",
